c5cb5017697f5b80932beca34bd089ce8214004a
Andre Paliao <andre_paliao@hotmail.com>
2018-01-14 02:31:05 -0200

    Setting up firebase

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import firebase from 'firebase'
 
 import { Header } from './src/components/common'
 
 export default class App extends React.Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCdsq-Wyegwi5Hvbvp17pEAgDdVoTPb2eo',
+      authDomain: 'paliao-auth.firebaseapp.com',
+      databaseURL: 'https://paliao-auth.firebaseio.com',
+      projectId: 'paliao-auth',
+      storageBucket: 'paliao-auth.appspot.com',
+      messagingSenderId: '867835900259'
+    })
+  }
+
   render() {
     return (
       <View>
